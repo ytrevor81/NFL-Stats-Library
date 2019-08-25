4fe4e24d24f4ca8645c703a313bb443deb97821b
Trevor Youngquist <ytrevor81@gmail.com>
2019-08-25 19:58:21 +0700

    Update settings.py

--- statsman/settings.py
+++ statsman/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tw=@gbua$$#sz^61g39t1em-p5acsc((clhn$)5#o31t^s(%b@'
+SECRET_KEY = ###Secret ;)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
