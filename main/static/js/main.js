78c662e79e60b16f11f1e2d0d6d0f24c8444bb95
Trevor Youngquist <46886041+ytrevor81@users.noreply.github.com>
2019-10-22 15:04:50 +0700

    finished responsive navbar!!!

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,7 +1,16 @@
 /*	==================================================
 	FILE: main.js
-	DESC: Custom JS/jQuery 
+	DESC: Custom JS/jQuery
  *	=============================================== */
-$(document).ready(function(){
-	
-});
+
+//Creates a slding nav bar by changing the 'transform' property in .col.middle from 100% to 0%
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.col.middle');
+
+	burger.addEventListener('click', () => {
+		$(nav).toggleClass("approved");
+	});
+};
+
+navSlide();
